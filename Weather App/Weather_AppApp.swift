eb346a6936b0fae70efe31d5d5792b01c03ab559
Gabriel Lopes <lopessantosgabriel@outlook.com>
2024-05-10 15:37:08 -0300

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Gabriel Lopes on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
